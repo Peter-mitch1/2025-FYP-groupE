761bb66d24cbf667eec123c08af8905acb49c04b
MichaelKayser91 <mikay@itu.dk>
2025-03-31 11:09:00 +0200

    Updated inpaint_util.py to support tophat for white/grey hair

--- util/inpaint_util.py
+++ util/inpaint_util.py
@@ -1,17 +1,21 @@
 import cv2
 
-
-def removeHair(img_org, img_gray, kernel_size=25, threshold=10, radius=3):
+def removeHair(img_org, img_gray, kernel_size=25, threshold=10, radius=3, method="blackhat"):
     # kernel for the morphological filtering
     kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (kernel_size, kernel_size))
 
-    # perform the blackHat filtering on the grayscale image to find the hair countours
-    blackhat = cv2.morphologyEx(img_gray, cv2.MORPH_BLACKHAT, kernel)
+    # perform the morphological filtering based on the method
+    if method == "blackhat":
+        transform = cv2.morphologyEx(img_gray, cv2.MORPH_BLACKHAT, kernel)
+    elif method == "tophat":
+        transform = cv2.morphologyEx(img_gray, cv2.MORPH_TOPHAT, kernel)
+    else:
+        raise ValueError("Method must be 'blackhat' or 'tophat'")
 
-    # intensify the hair countours in preparation for the inpainting algorithm
-    _, thresh = cv2.threshold(blackhat, threshold, 255, cv2.THRESH_BINARY)
+    # intensify the hair contours in preparation for the inpainting algorithm
+    _, thresh = cv2.threshold(transform, threshold, 255, cv2.THRESH_BINARY)
 
     # inpaint the original image depending on the mask
     img_out = cv2.inpaint(img_org, thresh, radius, cv2.INPAINT_TELEA)
 
-    return blackhat, thresh, img_out
+    return transform, thresh, img_out
